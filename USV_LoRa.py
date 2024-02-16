7b1d6e8efe3dcadcce4223bf375c0667daaa2e6a
maxwcurren <160265308+maxwcurren@users.noreply.github.com>
2024-02-16 15:34:32 -0800

    Update USV_LoRa.py

--- USV_LoRa.py
+++ USV_LoRa.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+# THIS IS THE LORA CODE FOR THE USV MODULE WRITTEN FOR THE REYAX RYLR896 Lora Module
+
 import serial
 import time
 import struct  # Import the struct module
@@ -24,9 +26,28 @@ def parse(sentence):
         mode = int(match.group(4))
         throttle = int(match.group(5))
         steering = int(match.group(6))
+
+        if throttle > 1023:
+            throttle = 1023
+        if steering > 1022:
+            steering = 1022
+            
+        if steering not in [1022, 510, 0]:
+            if steering == 102:
+                steering = 1022
+            else:  
+                steering = 510
         return req, mode, throttle, steering
     else:
         return None
+
+def transmit(data):
+    lora.write(('AT+SEND=99,' + str(len(data)) + ',' + data + ',\r\n').encode('utf-8'))
+    
+def receive():
+    lora.write(('+RCV\r\n').encode('utf-8'))
+    response = lora.readline().decode('utf-8').strip()
+    return response
 
 mode = 0
 req = 0
@@ -37,7 +58,7 @@ while True:
     # RECEIVE MANUAL CONTROLS OVER LORA
     
     time.sleep(0.1)
-    response = lora.readline().decode('utf-8').strip()
+    response = receive()
 
     # Filter response
     if 'ERR' not in response:
@@ -47,44 +68,30 @@ while True:
             result = parse(response)
             if result is not None:
                 req, mode, throttle, steering = result
-                if throttle > 1023:
-                    throttle = 1023
-                if steering > 1022:
-                    steering = 1022
-                    
-                if steering not in [1022, 510, 0]:
-                    if steering == 102:
-                        steering = 1022
-                    else:  
-                        steering = 510
-
                 print("mode is", mode)
-
                 if mode == 1:
                     # IN MANUAL MODE
+                    # Send motor controls to arduino
                     print("throttle is", throttle)
                     print("steering is", steering)
                     ser.write(struct.pack('<h', int(throttle)))
                     ser.flush()
                     ser.write(struct.pack('<h', int(steering)))
                     ser.flush()
-                    lora.write(('+RCV\r\n').encode('utf-8'))
                 elif mode == 0: 
-                    # IN AUTO MODE 
+                    # IN AUTO MODE
+                    # Send data if requested
                     if req == 1:
                         print("Data Request")
                         # Grab sensor values
                         # use dummy values:
-                        longitude = "524.32651"
-                        latitude = "-12.36521"
+                        longitude = "524"
+                        latitude = "-12"
                         orientation = "N"
                         data = '*' + longitude + '&' + latitude + '^' + orientation + '+'
                         time.sleep(0.1)
-                        ser.write(('AT+SEND=99,' + str(len(data)) + ',' + data + ',\r\n').encode('utf-8'))
+                        transmit(data)
                         print("Sent: ",data)
-                        response = ser.readline().decode('utf-8').strip()
-                        print(response)
-                        lora.write(('+RCV\r\n').encode('utf-8'))
                     else:
                         print("No Data request")
                         
