a6c35f07c1424615947d8c33a89dd78da57b023b
maxwcurren <160265308+maxwcurren@users.noreply.github.com>
2024-02-18 16:36:48 -0800

    Update PID.py

--- PID.py
+++ PID.py
@@ -5,10 +5,6 @@ import math
 import serial
 import re
 
-# Import sensor retrieve scripts
-import Accel_Mag
-import GPS
-
 def pid_controller(Kp_surge, Ki_surge, Kd_surge, Kp_yaw, Ki_yaw, Kd_yaw, setpoint_surge, setpoint_yaw, integral_surge, integral_yaw, prev_e_surge, prev_e_yaw, current_value_surge, current_value_yaw, dt):
     e_surge = setpoint_surge - current_value_surge
     integral_surge += e_surge * dt
@@ -58,64 +54,35 @@ def mapYaw(yaw):
     # MAPS YAW FROM 0 TO 1022 ALL EVEN NUMBERS
     pass
 
-def sendControls(surge,yaw):
+def sendPIDControls(surge,yaw):
     # NEED TO IMPLEMENT SEND TO ARDUINO
     throttle = mapSurge(surge)
     steering = mapYaw(yaw)
+    pass
     # send to arduino
 
-
-# Create Array of Waypoints
-# Choose number of waypoints
-num_waypoints = int(input("Enter the amount of waypoints you will be using: "))
-
-# Create array of waypoints
-waypoints_lon = []
-waypoints_lat = []
-for i in range(num_waypoints):
-    # Get longitude and latitude input from the user
-    # Will replace with better system once this code works
-    lon = float(input("Enter longitude for waypoint " + str(i + 1) + ": "))
-    lat = float(input("Enter latitude for waypoint " + str(i + 1) + ": ")) 
-        
-    # Append the coordinates as a list to the 2D array
-    waypoints_lon.append(lon)
-    waypoints_lat.append(lat)
-
-# Combine the lon and lat lists into a 2D array
-waypoints = list(zip(waypoints_lon, waypoints_lat))
-
-# Print the waypoints array
-print("Longitude\tLatitude")
-for row in range(num_waypoints):
-    print(f"{waypoints[row][0]}\t\t{waypoints[row][1]}")
-
-# PID parameters
-Kp_surge, Ki_surge, Kd_surge = 11.59, 2.86, 1.88
-Kp_yaw, Ki_yaw, Kd_yaw = 11.59, 2.86, 1.88
-dt = 0.1    # Rate of sensor updates
-# Initialize errors
-prev_e_surge = 0
-prev_e_yaw = 0
-
-QMC = QMC5883L()
-mpu6050 = MPU6050()
-
-while True:
-    for i in range(num_waypoints - 1):
-        # Read GPS Module for coordinates
-        gps_lon, gps_lat = getGPS()
-        # Read magnometer
-        current_yaw = getMagno()
-        # Read accelerometer
-        current_surge = getAccel()
-        setpoint_surge = 1.0  # Surge setpoint (m/s)
-        output_surge, output_yaw, integral_surge, integral_yaw = pid_controller(Kp_surge, Ki_surge, Kd_surge, Kp_yaw, Ki_yaw, Kd_yaw, setpoint_surge, setpoint_yaw, integral_surge, integral_yaw, prev_e_surge, prev_e_yaw, current_surge, current_yaw, dt)
-        
-        # MAP setpoints to 0 to 1022 for yaw and 0 to 1023 for surge and send to arduino for motor control
-        sendControls(output_surge,output_yaw)
-
-        setpoint_yaw = desired_heading(gps_lon, gps_lat, current_yaw, waypoints_lon[i], waypoints_lat[i])
-        prev_e_surge = setpoint_surge - current_surge
-        prev_e_yaw = setpoint_yaw - current_yaw
-        
+#def createWaypoints()
+#    # Choose number of waypoints
+#    num_waypoints = int(input("Enter the amount of waypoints you will be using: "))
+#    
+#    # Create array of waypoints
+#    waypoints_lon = []
+#    waypoints_lat = []
+#    for i in range(num_waypoints):
+#        # Get longitude and latitude input from the user
+#       # Will replace with better system once this code works
+#        lon = float(input("Enter longitude for waypoint " + str(i + 1) + ": "))
+#        lat = float(input("Enter latitude for waypoint " + str(i + 1) + ": ")) 
+#            
+#        # Append the coordinates as a list to the 2D array
+#        waypoints_lon.append(lon)
+#        waypoints_lat.append(lat)
+#    
+#    # Combine the lon and lat lists into a 2D array
+#    waypoints = list(zip(waypoints_lon, waypoints_lat))
+#    
+#    # Print the waypoints array
+#    print("Longitude\tLatitude")
+#    for row in range(num_waypoints):
+#        print(f"{waypoints[row][0]}\t\t{waypoints[row][1]}")
+#    return waypoints_lon, waypoints_lat
