42d5aa802c68c674014794ec5858d7c0276a3060
maxwcurren <160265308+maxwcurren@users.noreply.github.com>
2024-02-26 00:56:52 -0800

    Update USV.py

--- USV.py
+++ USV.py
@@ -1,28 +1,160 @@
-#!/usr/bin/env python
-# THIS IS THE MAIN CODE FOR THE USV
-
-# PID libraries
-import math
-import numpy as np
-from scipy.integrate import odeint
-
-# Sensor libraries
-import serial
 import time
+from Accel_Mag import QMC5883L, MPU6050
+import GPS
+import serial
+import Lidar
+import math
 import struct
 import re
 
-# Import Scripts
-import Accel_Mag
-import GPS
-import PID
-
-ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)  # set up serial output for Arduino
+QMC = QMC5883L()
+mpu6050 = MPU6050()
+ser = serial.Serial('/dev/ttyACM0', 1200, timeout=1)  # set up serial output for Arduino
 lora = serial.Serial('/dev/ttyS0', baudrate=115200, timeout=0)  # set up LoRa
 
-QMC = Accel_mag.QMC5883L()
-mpu6050 = Accel_mag.MPU6050()
+# Get starting position of USV
+starting_lon, starting_lat = GPS.getGPS()
+print(f"Starting Longitude: {starting_lon/10000.0}, Starting Latitude: {starting_lat/10000.0}")
 
+# create test variables:
+waypoint_lon = [33.89021, 33.89109, 33.89084, 33.88992, 33.88928]
+waypoint_lat = [-117.46711, -117.46649, -117.46769, -117.46800, -117.46750]
+waypoint_num = len(waypoint_lon)
+# return_method will determine how the USV returns to the base station. It will be determined on creation of waypoint arrays.
+# If return_method is zero then the USV will return to the starting position by iterating through the waypoints in reverse.
+# If return_method is one then the USV will directly calculate the heading towards the starting position and stop when it arrives.
+return_method = 1
+# Define globals
+waypoint_count = 0
+mode = 1
+req = 0
+throttle = 511
+steering = 511
+distances_prev = []
+distances = []
+angles_prev = []
+angles = []
+
+# Define PID variables:
+Kp_yaw, Kd_yaw = 2, 1.88
+prev_error = 0.0
+cumulative_error = 0.0
+
+# Read from accel and gyro 
+# Ax, Ay, Az = mpu6050.read_accelerometer_data()
+# Gx, Gy, Gz = mpu6050.read_gyroscope_data()
+
+# PATHING FUNCTIONS
+def detectObject(current_heading, distances, angles):
+    print(f"Current Heading: {current_heading}")
+    
+    # Find the difference between the current_heading and each angle in angles
+    angle_diff = [abs(current_heading - angle) for angle in angles]
+    
+    # Find the index of the minimum difference
+    min_diff_index = angle_diff.index(min(angle_diff))
+    
+    # Get the nearest angle and corresponding distance
+    nearest_angle = angles[min_diff_index]
+    nearest_distance = distances[min_diff_index]
+
+    if current_heading - 15 <= nearest_angle <= current_heading + 15:
+        # If there is an object detected in the current path of the USV
+        object_detected = 1
+        if nearest_angle <= current_heading:
+            # If the object is to the right of the threshold window or directly in front, then adjust heading of the USV to the left.
+            new_heading = (current_heading + 30) % 360
+        else:
+            # If the object is to the left of the threshold window adjust the heading of the USV to the right.
+            new_heading = (current_heading - 30) % 360
+    else:
+        object_detected = 0
+        new_heading = current_heading
+
+    #print(f"New Heading: {new_heading}")
+    print(f"Object Detected: {object_detected}")
+    return object_detected, new_heading
+
+def getNextHeading(current_heading, distances, angles, waypoint_lon, waypoint_lat, current_lon, current_lat, return_method):
+    global waypoint_count, waypoint_num, starting_lon, starting_lat
+    # Calculate distance to waypoint
+    delta_lon = waypoint_lon - current_lon
+    delta_lat = waypoint_lat - current_lat
+    distance_to_waypoint = math.sqrt((delta_lon)**2 + (delta_lat)**2)
+
+    if distance_to_waypoint < 10.0:
+        waypoint_count += 1
+
+    if waypoint_count <= waypoint_num:
+        object_detected, pot_heading = detectObject(current_heading, distances, angles)
+
+        if object_detected == 1:
+            new_heading = pot_heading
+        else:
+            pid_output = pid_controller(current_heading, waypoint_lon, waypoint_lat, current_lon, current_lat)
+            # Add saturation limits to prevent sudden adjustments
+            # pid_output = max(min(pid_output, max_output), min_output)
+            new_heading = pid_output
+
+        return new_heading
+
+    # Check if it's time to return
+    elif waypoint_count == waypoint_num + 1:
+        if return_method == 0:  # Iterate through waypoints in reverse
+            waypoint_count -= 1
+            object_detected, pot_heading = detectObject(current_heading, distances, angles)
+
+            if object_detected == 1:
+                new_heading = pot_heading
+            else:
+                pid_output = pid_controller(current_heading, starting_lon, starting_lat, current_lon, current_lat)
+                new_heading = current_heading + pid_output
+
+            return new_heading
+
+        elif return_method == 1:  # Directly calculate heading towards starting position
+            delta_starting_lon = starting_lon - current_lon
+            delta_starting_lat = starting_lat - current_lat
+            return_heading = math.atan2(delta_starting_lon, delta_starting_lat)
+            return return_heading
+
+def pid_controller(current_heading, waypoint_lon, waypoint_lat, current_lon, current_lat):
+    global prev_error, Kp_yaw, Kd_yaw
+    
+    # Calculate the differences in x and y coordinates
+    delta_lon, delta_lat = waypoint_lon - current_lon, waypoint_lat - current_lat
+    
+    # Calculate the yaw angle using arctangent
+    target_heading = math.atan2(delta_lon, delta_lat)
+    
+    # Normalize the target_heading to the range [0, 360)
+    target_heading_degrees = (math.degrees(target_heading) + 360) % 360
+    print(f"Ideal Heading: {target_heading_degrees}")
+    # Calculate the error
+    error = target_heading_degrees - current_heading
+    
+    # Calculate the PD components
+    proportional = Kp_yaw * error
+    derivative = Kd_yaw * (error - prev_error)
+    print(f"Proportional: {proportional}")
+    print(f"Derivative: {derivative}")
+    
+    # Calculate the PD output
+    pid_output = proportional + derivative
+    # Update previous error
+    prev_error = error
+    
+    return pid_output
+
+def mapYaw(yaw):
+    # MAPS YAW FROM 0 TO 1022 ALL EVEN NUMBERS
+    print(f"yaw: {yaw}")
+    mapped_value = int(yaw * (1022 / 360))
+    mapped_value = mapped_value - (mapped_value % 2)
+    print(f"Mapped Steering: {mapped_value}")
+    return mapped_value
+
+# LORA FUNCTIONS
 def parse(sentence):
     # The format is "+RCV=ADDRESS,MESSAGE LENGTH,*request&mode^throttle%steering+"
     match = re.match(r'\+RCV=(\d+),(\d+),\*(\d+)&(\d+)\^(\d+)\%(\d+)\+', sentence)
@@ -37,7 +169,7 @@ def parse(sentence):
         if steering > 1022:
             steering = 1022
 
-        if steering not in [1022, 510, 0]:
+        if steering not in [1022, 510, 2]:
             if steering == 102:
                 steering = 1022
             else:
@@ -60,7 +192,7 @@ def transmit_Lora(data, max_retries=15):
             break  # Successful acknowledgment, exit the loop
         else:
             retry_count += 1
-            print(f"Retrying... (Retry {retry_count}/{max_retries})")
+            #print(f"Retrying... (Retry {retry_count}/{max_retries})")
             time.sleep(0.1)
 
     if retry_count == max_retries:
@@ -74,81 +206,89 @@ def receive_Lora():
 def send_arduino(throttle, steering):
     ser.write(struct.pack('<h', int(throttle)))
     ser.flush()
+    print(f"Steering Sent to Arduino: {steering}")
     ser.write(struct.pack('<h', int(steering)))
     ser.flush()
 
-mode = 0
-req = 0
-throttle = 511
-steering = 511
+def manual():
+    global throttle, steering
+    # IN MANUAL MODE
+    # Send motor controls to Arduino
+    #print("throttle is", throttle)
+    #print("steering is", steering)
+    send_arduino(throttle, steering)
+def auto():
+    global throttle, steering, req, distances, distances_prev, angles, angles_prev
+    # IN AUTO MODE
+    throttle = 711
+    # Read GPS Module for coordinates
+    time_before = time.time()
+    gps_lon, gps_lat = GPS.getGPS()
+    gps_lon1 = gps_lon / 10000
+    gps_lat1 = gps_lat / 10000
+    # Read magnetometer
+    current_yaw = QMC.get_bearing()
 
-#Get Waypoints:
-print("Implement get starting position and waypoints from Base Station")
-# will be a transmit from the base station and confirmation from the USV
+    # Check for objects
+    distances, angles = Lidar.scan_lidar()
+    time_after = time.time()
+    sensor_time = time_after - time_before
+    #print(f"Ack Time: {sensor_time}")
+    if len(distances) == 0:
+        distances = distances_prev.copy()  # Make a deep copy
+        angles = angles_prev.copy()  # Make a deep copy
+    else:
+        distances_prev = distances.copy()  # Make a deep copy
+        angles_prev = angles.copy()  # Make a deep copy
 
-# Set PID Parameters:
-Kp_surge, Ki_surge, Kd_surge = 11.59, 2.86, 1.88
-Kp_yaw, Ki_yaw, Kd_yaw = 11.59, 2.86, 1.88
-dt = 0.1    # Rate of sensor updates
-# Initialize errors
-prev_e_surge = 0
-prev_e_yaw = 0
+    # Get target yaw using PID or object detected function
+    target_yaw = getNextHeading(current_yaw, distances, angles, waypoint_lon[waypoint_count], waypoint_lat[waypoint_count], gps_lon1, gps_lat1, return_method)
+    #print(f"Target Yaw: {target_yaw}")
+    # print(f"Waypoint Count: {waypoint_count}")
+    # MAP setpoints to 0 to 1022 for yaw and send to Arduino for motor control
+    steering = mapYaw(target_yaw)
+
+    # Send motor controls to Arduino
+    send_arduino(throttle, steering)
+
+    #print(distances)
+    #print(angles)
+    # print("Gx={:.2f} °/s \tGy={:.2f} °/s \tGz={:.2f} °/s \tAx={:.2f} g \tAy={:.2f} g \tAz={:.2f} g \tBearing: {:.2f} degrees".format(Gx, Gy, Gz, Ax, Ay, Az, bearing))
+    # print(f"Bearing: {current_yaw} degrees")
+    #print(f"Longitude: {gps_lon1}, Latitude: {gps_lat1}")
+
+    # Send data if requested
+    if req == 1:
+        print("Data Request")
+        data = '*' + str(gps_lon) + '&' + str(gps_lat) + '^' + str(math.floor(current_yaw)) + '+'            
+        # Wait for ACK before moving on
+        transmit_Lora(data)
+        req = 0
+    else:
+        print("No Data request")
 
 while True:
     # RECEIVE MANUAL CONTROLS OVER LORA
     time.sleep(0.1)
     response = receive_Lora()
-
+    #print(f"Response{response}")
     # Filter response
     if 'ERR' not in response:
-        print(f"Unfiltered response: {response}")
         if '%' in response and '^' in response and '&' in response and '+' in response and '*' in response:
             # If data successfully sent over LoRa
             #print(response)
             result = parse(response)
             if result is not None:
                 req, mode, throttle, steering = result
-                print("mode is", mode)
+                if mode == 0:
+                    print("Auto Mode")
+                else:
+                    print("Manual Mode")
+                print("throttle is", throttle)
+                print("steering is", steering)
                 if mode == 1:
-                    # IN MANUAL MODE
-                    # Send motor controls to Arduino
-                    print("throttle is", throttle)
-                    print("steering is", steering)
-                    send_arduino(throttle, steering)
+                    manual()
                 elif mode == 0:
-                    # IN AUTO MODE
-                    for i in range(num_waypoints - 1):
-                        # Read GPS Module for coordinates
-                        gps_lon, gps_lat = GPS.getGPS()
-                        gps_lon1 = gps_lon / 10000
-                        gps_lat1 = gps_lat / 10000
-                        # Read magnometer
-                        current_yaw = Accel_Mag.getMagno()
-                        # Read accelerometer
-                        current_surge = Accel_Mag.getAccel()
-                        setpoint_surge = 1.0  # Surge setpoint (m/s)
-                        output_surge, output_yaw, integral_surge, integral_yaw = PID.pid_controller(Kp_surge, Ki_surge, Kd_surge, Kp_yaw, Ki_yaw, Kd_yaw, setpoint_surge, setpoint_yaw, integral_surge, integral_yaw, prev_e_surge, prev_e_yaw, current_surge, current_yaw, dt)
-                        
-                        # MAP setpoints to 0 to 1022 for yaw and 0 to 1023 for surge and send to arduino for motor control
-                        #NOT YET IMPLEMENTED
-                        print("Function sendPIDControls not yet implemented")
-                        PID.sendPIDControls(output_surge,output_yaw)
+                    auto()
 
-                        setpoint_yaw = PID.desired_heading(gps_lon1, gps_lat1, current_yaw, waypoints_lon[i], waypoints_lat[i])
-                        prev_e_surge = setpoint_surge - current_surge
-                        prev_e_yaw = setpoint_yaw - current_yaw
                         
-                        # Send data if requested
-                        if req == 1:
-                            print("Data Request")
-                            # Grab sensor values
-                            # use dummy values:
-                            longitude, latitude = GPS.getGPS()
-                            orientation = math.floor(current_yaw)
-                            data = '!' + str(gps_lon) + '&' + str(gps_lat) + '^' + orientation + '+'
-                            time.sleep(0.1)
-                        
-                        # Wait for ACK before moving on
-                        transmit_Lora(data)
-                    else:
-                        print("No Data request")
