4a10d26b68d14485e23e71702b0c6bd2652e9d28
maxwcurren <160265308+maxwcurren@users.noreply.github.com>
2024-03-02 05:25:43 -0800

    Lidar.py

--- LIDAR_A1.py
+++ LIDAR_A1.py
@@ -1,40 +1,39 @@
-import matplotlib.pyplot as plt
-import numpy as np
-from math import cos, sin, pi, floor
+from math import floor
 from adafruit_rplidar import RPLidar
+import time
 
 # Setup the RPLidar
-PORT_NAME = "/dev/ttyUSB0"
-lidar = RPLidar(None, PORT_NAME, timeout=3)
+PORT_NAME = "/dev/ttyUSB1"
+lidar = RPLidar(None, '/dev/ttyUSB1', timeout=3)
 
-# used to scale data to fit on the screen
-
-def scan_lidar():
+def scan_lidar(duration):
+    lidar.connect()
+    time.sleep(0.1)
     AngleArr = []
     DistanceArr = []
-    scan_data = [0] * 360
-    scan_counter = 0
+    start_time = time.time()
+
     try:
         for scan in lidar.iter_scans():
             for _, angle, distance in scan:
-                scan_data[min([359, floor(angle)])] = distance
+                if time.time() - start_time > duration:
+                    raise StopIteration  # Stop the iteration after the specified duration
+                AngleArr.append(angle)
                 DistanceArr.append(distance)
-                AngleArr.append(angle)    
-            scan_counter += 1
-            
-            print("Distance array =", DistanceArr)
-            print("Angle Array = ", AngleArr)
-            AngleArr = []
-            DistanceArr = []
-            
-    except KeyboardInterrupt:
-        print("Stopping.")
+
+    except StopIteration:
+        pass  # This exception is caught to exit the loop when the specified duration is reached
+    finally:
         lidar.stop()
         lidar.disconnect()
-    
-        restart = input("waiting  for enter")
-        if restart == "":
-            lidar.connect()
-            scan_lidar()
-scan_lidar()
 
+    return DistanceArr, AngleArr
+
+if __name__ == "__main__":
+    scan_duration = 2  # Set the duration in seconds
+    distances, angles = scan_lidar(scan_duration)
+
+    print(len(distances))
+    print(len(angles))
+    print(f"distances: {distances}")
+    print(f"angles: {angles}")
