54dd93ae852320908c743e6e0772110b78bb37e4
maxwcurren <160265308+maxwcurren@users.noreply.github.com>
2024-03-19 15:28:31 -0700

    Update and rename bad_esc_control.ino to BoatEscController.ino

--- USV/BoatEscController.ino
+++ USV/BoatEscController.ino
@@ -17,7 +17,7 @@ void loop() {
       int lastVal=0;
       Serial.readBytes((byte*)&receivedValue, sizeof(int));
       Serial.flush();
-      Serial.println(receivedValue);
+      //Serial.println(receivedValue);
       // Send the processed value back to the Raspberry Pi
       // Serial.write((byte*)&receivedValue, sizeof(int));
       // Serial.flush();
@@ -31,16 +31,16 @@ void loop() {
       else{ 
         if (receivedValue % 2 == 1) {
           EscInput=receivedValue;
-          Serial.print("EscInput: ");
-          Serial.print(EscInput);
-          Serial.print("\n");
+          //Serial.print("EscInput: ");
+          //Serial.print(EscInput);
+          //Serial.print("\n");
           //Serial.write((char*)&EscInput, sizeof(int));
         }
         if (receivedValue % 2 == 0) {
           ServoInput=receivedValue;
-          Serial.print("ServoInput: ");
-          Serial.print(ServoInput);
-          Serial.print("\n");
+          //Serial.print("ServoInput: ");
+          //Serial.print(ServoInput);
+          //Serial.print("\n");
         }
         lastVal=receivedValue;
       }
@@ -50,6 +50,13 @@ void loop() {
     
   int throttle = map(EscInput, 0, 1024, 1100, 1800); //maps input 0-1023 to output
   int steering = map(ServoInput, 0, 1024, 1000, 2000);
+  
+  // Ensure motors don't get out of range value
+  if (throttle > 600) {throttle = 585;}
+  else if (throttle < 410) {throttle = 410}
+  if (steering > 1022) {steering = 1022;}
+  else if (steering < 2) {steering = 2;}
+  
   // 1-->2 ms delay min-->max speed esc
     digitalWrite(11, HIGH);
     delayMicroseconds(throttle);
